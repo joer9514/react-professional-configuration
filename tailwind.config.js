9b5eabd0985c8cacbb6c068a474229f8b5798b57
joer9514 <joer9514@gmail.com>
2023-09-29 17:11:11 -0500

    feat: Updated some dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
-module.exports = {
+export default {
   darkMode: 'class',
   important: 'html',
   content: [
     'index.html',
     './src/App.tsx',
     './src/components/**/*.{tsx,jsx,js,css,html}',
+    './src/routes/**/*.{tsx,jsx,js,css,html}',
     './src/pages/**/components/**/*.{tsx,jsx,js,css,html}',
   ],
   theme: {
